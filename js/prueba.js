77735e12109d0e7d58fc83d611ff2d2924f0a2dc
Santiago Chango <sachango@uce.edu.ec>
2023-12-06 19:45:24 -0500

    main

---
+++ js/prueba.js
@@ -0,0 +1,11 @@
+Math.random()
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+function pasar(){
+
+    document.getElementById('1').innerText=10;
+    document.getElementById('id1').style.fontSize= '20px';
+}
